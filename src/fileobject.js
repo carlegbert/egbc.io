dc10c78c1ea9a3bb08e4550ced1ea5dcc7e06ad0
Carl Egbert <egbertcarl@gmail.com>
2016-12-25 21:49:52 -0800

    create FileObject class & subclasses

---
+++ src/fileobject.js
@@ -0,0 +1,53 @@
+class FileObject {
+  constructor(name, fullPath, filetype, parentRef) {
+    this.name = name;
+    this.fullPath = fullPath;
+    this.filetype = filetype;
+    this.parentRef = parentRef;
+  }
+
+  jsonToFile(json) {
+    const newFile = new FileObject(json.name, json.fullPath, json.filetype, json.parentRef);
+    if (newFile.filetype === 'dir') {
+      newFile.children = [];
+      json.children.forEach((child) => {
+        const childFile = this.jsonToFile(child);
+        childFile.parentRef = newFile;
+        newFile.children.push(childFile);
+      });
+    }
+    return newFile;
+  }
+
+  getLsEntry() {
+    return `<span class="dir ${this.filetype}" id="${this.fullPath}">${this.name}</span>`;
+  }
+
+}
+
+class DirFile extends FileObject {
+  constructor(name, fullPath, filetype, parentRef, contents) {
+    super(name, fullPath, filetype, parentRef);
+    this.contents = contents;
+  }
+
+  /* returns list of files that match up with anything
+   * in an string array passed to the function */
+  getContentsByTypes(types) {
+    const files = [];
+    this.contents.forEach((child) => {
+      if (types.includes(child.filetype)) {
+        files.push(child);
+      }
+    });
+  }
+
+}
+
+class TxtFile extends FileObject {
+  constructor(name, fullPath, filetype, parentRef, children) {
+    super(name, fullPath, filetype, parentRef);
+    this.children = children;
+  }
+
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+require('./fileobject.js');
