8ab60138e24dd36b2b8e0e2afd890993ad6bd5a5
Carl Egbert <egbertcarl@gmail.com>
2018-01-07 19:45:45 -0800

    update content

--- src/content.js
+++ src/content.js
@@ -13,7 +13,7 @@ links.children = [
 const about = homeDir.createChild(['~', 'about.txt']);
 
 about.contents = [
-  '<p>Thanks for visiting my personal website. I\'m currently employed as a QA intern at Incomm in Portland, OR, primarily writing automated UI testing and related tooling, but also undertaking general QA duties. I\'m interested in further opportunities in test automation as well as fullstack and backend web development. I\'m comfortable with JS and Python and also have some familiarity with C#, and am generally unafraid of learning new languages, frameworks, and tools.</p>',
+  '<p>Thanks for visiting my personal website. I\'m currently employed as a QA engineer Incomm in Portland, OR, where I work on test and build automation and perform manual QA. I\'m not interested in employment opportunities at this time. I\'m comfortable with JS and Python and also have some familiarity with C#, and am generally unafraid of learning new languages, frameworks, and tools.</p>',
   '<p>Before beginning my career in the tech industry, I was a professional classical double bassist. I\'ve performed with the Oregon and Eugene Symphonies, the Chintimini Chamber Music Festival, the Aspen Festival Orchestra, and many other professional ensembles.</p>',
   '<ul><li><a href="mailto:egbertcarl@gmail.com">egbertcarl@gmail.com</a></li><li><a href="https://www.linkedin.com/in/carlegbert" target="blank">linkedin</a></li><li><a href="https://github.com/carlegbert" target="blank">github</a></li><li><a href="https://github.com/carlegbert/wwwterm-es6" target="blank">this site on gh</a></li></ul>',
   '<p><span class="underline">About this site</span></p>',
