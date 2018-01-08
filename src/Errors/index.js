401c808d4646a7f1f94dfdd6f118a97fd0ae7db3
Carl Egbert <egbertcarl@gmail.com>
2018-01-07 17:21:59 -0800

    remove unused error classes

--- src/Errors/index.js
+++
@@ -1,9 +0,0 @@
-const InvalidFileError = require('./InvalidFileError');
-const FileExistsError = require('./FileExistsError');
-const FileNotFoundError = require('./FileNotFoundError');
-
-module.exports = {
-  InvalidFileError,
-  FileExistsError,
-  FileNotFoundError,
-};
