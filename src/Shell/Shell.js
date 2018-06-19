6e93ea30c3aa9991eec4507664229a837c2f0da8
Carl Egbert <egbertcarl@gmail.com>
2018-06-18 19:40:08 -0700

    start refactoring autocomplete into its own module

--- src/Shell/Shell.js
+++ src/Shell/Shell.js
@@ -3,6 +3,7 @@ const { getElementById } = require('../util/selectors');
 const { Directory, File, Path } = require('../FileStructure');
 const ShellCommand = require('./ShellCommand');
 const ShellCommandResult = require('./ShellCommandResult');
+const autocomplete = require('./autocomplete');
 
 /**
  * programs.help cannot be exported in programs/index.js due to requiring
@@ -169,70 +170,17 @@ class Shell {
       options = PROGRAM_NAMES;
     } else if (!spaceAtEnd && cmd.args.length === 0) {
       partial = cmd.command;
-      options = Shell.filterAutoCompleteOptions(partial, PROGRAM_NAMES);
+      options = autocomplete.filterOptions(partial, PROGRAM_NAMES);
     } else {
       partial = cmd.args[cmd.args.length - 1] || '';
-      options = this.getAutocompleteFiles(partial, getValidTypesForProgram(cmd.command));
-      if (options.length === 0) options = this.getAutocompleteFiles(partial, [Directory]);
+      const typedPath = partial.split('/');
+      const partialName = typedPath.pop();
+      const dir = this.currentDir.findFile(typedPath, Directory);
+      options = autocomplete.getFiles(partialName, getValidTypesForProgram(cmd.command), dir);
+      if (options.length === 0) options = autocomplete.getFiles(partialName, [Directory], dir);
     }
     if (options.length === 1) this.completeWord(partial, options[0]);
     else if (options.length > 1) this.printAutoCompleteOptions(options);
-  }
-
-  /**
-   * Filter array of strings into options that match up with partial argument.
-   * @param {string} partial String to be autocompleted
-   * @param {string[]} options List of files or commands to check against partial
-   * @return {string[]} Array of strings from options that match against partial
-   */
-  static filterAutoCompleteOptions(partial, options) {
-    const len = partial.length;
-    const validOptions = options.filter(opt =>
-      (opt.length >= len && opt.slice(0, len) === partial));
-    const longestPartial = Shell.checkSuggestionsForSameBeginning(partial, validOptions);
-    if (longestPartial === partial) return validOptions;
-    return [longestPartial];
-  }
-
-  /**
-   * Make sure a list of different autocomplete options aren't identical up to a
-   * certain amount of letters, eg, if there are two options 'catalogue' and 'catamaran',
-   * and a user types 'vi c <Tab>', the input should be completed to 'cata'.
-   * @param {string} partial Original user input to be autocompleted
-   * @param {string[]} options Available options
-   * @return {string} A string representing the longest matching beginning.
-   * */
-  static checkSuggestionsForSameBeginning(partial, options) {
-    const sortedOpts = options.slice().sort((a, b) => a.length - b.length);
-    const shortestOpt = sortedOpts[0];
-    let longestPartial = partial;
-    for (let i = partial.length; i < shortestOpt.length; i += 1) {
-      const charToCheckFor = shortestOpt[i];
-      for (let j = 1; j < options.length; j += 1) {
-        if (options[j][i] !== charToCheckFor) return longestPartial;
-      }
-      longestPartial += shortestOpt[i];
-    }
-    return longestPartial;
-  }
-
-  /**
-   * returns list of all files in a directory for autocompletion purposes.
-   * @param {string} partial Filepath to be completed, eg, 'path/to/fi' or 'pat'
-   * @param {Class[]} filetypes Optional filetypes to filter for
-   * @return {string[]} array of filenames
-   */
-  getAutocompleteFiles(partial, filetypes) {
-    const partialAsArray = partial.split('/');
-    const partialName = partialAsArray.slice(-1)[0];
-    const dirPath = partialAsArray.slice(0, -1);
-    const dir = this.currentDir.findFile(dirPath, Directory);
-    const fileOptions = dir.getChildrenByTypes(filetypes);
-    /* eslint-disable arrow-body-style */
-    const options = fileOptions
-      .map((f) => { return (f instanceof Directory) ? `${f.name}/` : f.name; });
-    /* eslint-enable arrow-body-style */
-    return Shell.filterAutoCompleteOptions(partialName, options);
   }
 
   /**
---
+++ src/Shell/autocomplete.js
@@ -0,0 +1,57 @@
+const { Directory } = require('../FileStructure');
+
+/**
+ * Make sure a list of different autocomplete options aren't identical up to a
+ * certain amount of letters, eg, if there are two options 'catalogue' and 'catamaran',
+ * and a user types 'vi c <Tab>', the input should be completed to 'cata'.
+ * @param {string} partial Original user input to be autocompleted
+ * @param {string[]} options Available options
+ * @return {string} A string representing the longest matching beginning.
+ * */
+const checkSuggestionsForSameBeginning = (partial, options) => {
+  const sortedOpts = options.slice().sort((a, b) => a.length - b.length);
+  const shortestOpt = sortedOpts[0];
+  if (!shortestOpt) return partial;
+  let longestPartial = partial;
+  for (let i = partial.length; i < shortestOpt.length; i += 1) {
+    const charToCheckFor = shortestOpt[i];
+    for (let j = 1; j < options.length; j += 1) {
+      if (options[j][i] !== charToCheckFor) return longestPartial;
+    }
+    longestPartial += shortestOpt[i];
+  }
+  return longestPartial;
+};
+
+/**
+ * Filter array of strings into options that match up with partial argument.
+ * @param {string} partial String to be autocompleted
+ * @param {string[]} options List of files or commands to check against partial
+ * @return {string[]} Array of strings from options that match against partial
+ */
+const filterOptions = (partial, options) => {
+  const validOptions = options.filter(opt => opt.startsWith(partial));
+  const longestPartial = checkSuggestionsForSameBeginning(partial, validOptions);
+  if (longestPartial === partial) return validOptions;
+  return [longestPartial];
+};
+
+/**
+ * returns list of all files in a directory for autocompletion purposes.
+ * @param {string} partial Filepath to be completed, eg, 'path/to/fi' or 'pat'
+ * @param {Class[]} filetypes Optional filetypes to filter for
+ * @return {string[]} array of filenames
+ */
+const getFiles = (partial, filetypes, dir) => {
+  const fileOptions = dir.getChildrenByTypes(filetypes);
+  /* eslint-disable arrow-body-style */
+  const options = fileOptions
+    .map((f) => { return (f instanceof Directory) ? `${f.name}/` : f.name; });
+  /* eslint-enable arrow-body-style */
+  return filterOptions(partial, options);
+};
+
+module.exports = {
+  filterOptions,
+  getFiles,
+};
